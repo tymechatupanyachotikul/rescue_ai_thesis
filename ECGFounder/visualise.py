951f78c9632fbb3c7a320ce2efc01b9c1b8e58c9
Tyme Chatupanyachotikul <122781615+tymechatupanyachotikul@users.noreply.github.com>
2026-02-23 17:40:15 +0100

    .

--- ECGFounder/visualise.py
+++ ECGFounder/visualise.py
@@ -17,8 +17,17 @@ def save_umap_reduction(splits, save_dir):
         splits_out.append(np.array([split] * len(labels[-1])))
 
     embeddings = np.concatenate(embeddings, axis=0)
-    labels = np.concatenate(labels, axis=0)
+    labels = np.concatenate(labels, axis=0).flatten()
     splits_out = np.concatenate(splits_out, axis=0)
+
+    embeddings_healthy = embeddings[labels >= 60]
+    embeddings_unhealthy = embeddings[labels <= 50]
+
+    split_healthy = splits_out[labels >= 60]
+    split_unhealthy = splits_out[labels <= 50] 
+
+    labels_healthy = labels[labels >= 60]
+    labels_unhealthy = labels[labels <= 50]
 
     reducer = umap.UMAP(
         n_neighbors=30,
@@ -31,82 +40,92 @@ def save_umap_reduction(splits, save_dir):
 
     print(f'Performing UMAP reduction')
     embedding_2d = reducer.fit_transform(embeddings)
+    embedding_2d_healthy = reducer.fit_transform(embeddings_healthy)
+    embedding_2d_unhealthy = reducer.fit_transform(embeddings_unhealthy)    
     print(f'UMAP reduction completed')
 
     np.savez_compressed(
         os.path.join(save_dir, f'{"_".join(splits)}_embeddings.npz'), 
         embeddings=embedding_2d, 
         labels=labels,
-        splits=splits_out
+        splits=splits_out,
+        embeddings_healthy=embedding_2d_healthy,
+        labels_healthy=labels_healthy,
+        splits_healthy=split_healthy,
+        embeddings_unhealthy=embedding_2d_unhealthy,
+        labels_unhealthy=labels_unhealthy,
+        splits_unhealthy=split_unhealthy
     )
     print('Saved UMAP embeddings')
 
-# def load_and_plot_umap(file_path):
-#     data = np.load(file_path)
+def load_and_plot_umap(file_path):
+    data = np.load(file_path)
 
-#     embedding_2d = data['embeddings']
-#     labels = data['labels']
-#     splits = data['splits']
+    embedding_2d = data['embeddings']
+    labels = data['labels']
 
-#     print(f"Loaded {embedding_2d.shape[0]} samples.")
+    print(f"Loaded {embedding_2d.shape[0]} samples.")
 
-#     sns.set_context("talk")
-#     plt.figure(figsize=(12, 9), dpi=150)
+    sns.set_context("talk")
+    plt.figure(figsize=(12, 9), dpi=150)
 
-#     sc = plt.scatter(
-#         embedding_2d[:, 0], 
-#         embedding_2d[:, 1], 
-#         c=labels, 
-#         cmap='Spectral', 
-#         s=10, 
-#         alpha=0.7,
-#         edgecolors='none'
-#     )
+    sc = plt.scatter(
+        embedding_2d[:, 0], 
+        embedding_2d[:, 1], 
+        c=labels, 
+        cmap='Spectral', 
+        s=10, 
+        alpha=0.4,
+        edgecolors='none'
+    )
 
-#     cbar = plt.colorbar(sc)
-#     cbar.set_label('LVEF (%)', fontweight='bold', rotation=270, labelpad=20)
+    cbar = plt.colorbar(sc)
+    cbar.set_label('LVEF (%)', fontweight='bold', rotation=270, labelpad=20)
 
-#     plt.xlabel('UMAP Dimension 1', fontweight='bold')
-#     plt.ylabel('UMAP Dimension 2', fontweight='bold')
+    plt.xlabel('UMAP Dimension 1', fontweight='bold')
+    plt.ylabel('UMAP Dimension 2', fontweight='bold')
 
-#     sns.despine()
-#     plt.grid(True, linestyle='--', alpha=0.15)
+    sns.despine()
+    plt.grid(True, linestyle='--', alpha=0.15)
 
-#     plot_save_path = file_path.replace('.npz', '_train_plot.png')
-#     plt.savefig(plot_save_path, bbox_inches='tight')
-#     print(f"Plot saved to: {plot_save_path}")
+    plot_save_path = file_path.replace('.npz', '_train_plot.png')
+    plt.savefig(plot_save_path, bbox_inches='tight')
+    print(f"Plot saved to: {plot_save_path}")
 
-#     plt.show()
+    plt.show()
 
-# def plot_split_umap(file_path):
-#     data = np.load(file_path)
+def plot_split_umap(file_path):
+    data = np.load(file_path)
 
-#     embedding_2d = data['embeddings']
-#     splits = data['splits']
+    embedding_2d = data['embeddings']
+    splits = data['splits']
 
-#     plt.figure(figsize=(10, 8))
+    plt.figure(figsize=(10, 8))
     
-#     colors = sns.color_palette("Set1", n_colors=2)
+    colors = sns.color_palette("Set1", n_colors=2)
     
-#     for i, category in enumerate(['train', 'test']):
-#         mask = (splits == category)
-#         plt.scatter(
-#             embedding_2d[mask, 0], 
-#             embedding_2d[mask, 1], 
-#             c=[colors[i]], 
-#             label=category,
-#             s=12, 
-#             alpha=0.6,
-#             edgecolors='none'
-#         )
+    for i, category in enumerate(['train', 'test']):
+        mask = (splits == category)
+        plt.scatter(
+            embedding_2d[mask, 0], 
+            embedding_2d[mask, 1], 
+            c=[colors[i]], 
+            label=category,
+            s=12, 
+            alpha=0.6,
+            edgecolors='none'
+        )
 
-#     plt.xlabel('UMAP 1')
-#     plt.ylabel('UMAP 2')
+    plt.xlabel('UMAP Dimension 1', fontweight='bold')
+    plt.ylabel('UMAP Dimension 2', fontweight='bold')
     
-#     plt.legend(markerscale=2, frameon=True)
-    
-#     sns.despine()
-#     plt.show()
+    plt.legend(markerscale=2, frameon=True)
+    sns.despine()
+    plt.grid(True, linestyle='--', alpha=0.15)
+
+    plot_save_path = file_path.replace('.npz', '_split_plot.png')
+    plt.savefig(plot_save_path, bbox_inches='tight')
+    plt.show()
 
 directories = ['linear_probe_ft', 'full_ft']
 for directory in directories:
@@ -114,5 +133,7 @@ for directory in directories:
 
     #save_dir = f'/Users/tyme/Desktop/University/Thesis/rescue_ai/results/LVEF/embeddings/{directory}'
     save_dir = f'/home/tchatupanyacho/rescue_ai_thesis/results/LVEF/embeddings/{directory}'
-    save_umap_reduction(['test', 'train'], save_dir)
+    save_umap_reduction(['test'], save_dir)
             
+#plot_split_umap('/Users/tyme/Desktop/University/Thesis/rescue_ai/results/LVEF/embeddings/full_ft/test_train_embeddings.npz')
+#load_and_plot_umap('/Users/tyme/Desktop/University/Thesis/rescue_ai/results/LVEF/embeddings/linear_probe_ft/test_embeddings.npz')
