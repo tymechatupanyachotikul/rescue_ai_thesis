f41e16fcf1c1680a53a1d7cd1e57cf1e9d0b8f5e
Tyme Chatupanyachotikul <122781615+tymechatupanyachotikul@users.noreply.github.com>
2026-03-16 16:03:27 +0100

    .

--- MoNODE/data/aladin_preprocess.py
+++ MoNODE/data/aladin_preprocess.py
@@ -1,4 +1,5 @@
 import json
+import random
 import time
 import pandas as pd 
 import os
@@ -39,29 +40,47 @@ def analyse_single_case(record):
     aladin.reflection.batch(record)
     print("Reflection", time.time()-st)
 
-def plot_segment(record, original_record, case):
+def segment(record, original_record, case, segment_type, out_dir):
 
-    ecg = original_record.p_signal[:, 1]
-    fig, ax = plt.subplots(1, 1, figsize=(len(ecg)/(record.fs), 4), dpi=200)
-    ax.plot(np.arange(0, len(ecg)), ecg, color='black', label="ECG Signal")
+    #get segments
+    if segment_type == 'ventricular':
+        qrst_idx = []
+        for beat in record.qrs:
+            start = int(beat.onset)
+            if beat.t is not None:
+                end = int(beat.t.offset)
+            else:
+                continue
 
-    pcolor = '#e74c3c'
+            qrst_idx.append((start, end))
 
-    ymax = np.max(ecg)
-    ymin = np.min(ecg)
+        segments =random.sample(qrst_idx, 2)
+    elif segment_type == 'atrial':
+        segments = random.sample(record.p, 2)
+        segments = [(int(seg.onset), int(seg.offset)) for seg in segments]
 
-    for beat in record.qrs:
-        qrscolor = '#f1c40f' if beat.abnormal else '#2ecc71'
+    # normalise
+    mu = np.mean(original_record.p_signal, axis=0, keepdims=True)
+    sigma = np.std(original_record.p_signal, axis=0, keepdims=True)
 
-        start = int(beat.onset)
-        if beat.t is not None:
-            end = int(beat.t.offset)
-        else:
-            continue
-        ax.axvspan(start, end, ymax = ymax, ymin = ymin, color=qrscolor, alpha=0.5)
+    norm_ecg = (original_record.p_signal - mu) / (sigma + 1e-8)
 
-    for wave in record.p:
-        ax.axvspan(wave.onset, wave.offset, ymax = ymax, ymin = ymin, color=pcolor, alpha=0.5)
+    save_dir = os.path.join(out_dir, 'sampled')
+    for idx, segment in enumerate(segments):
+        ecg_segment = norm_ecg[segment[0]: segment[1], :]
+
+        delta_t = segment[1] - segment[0]
+        np.save(os.path.join(save_dir, f'T{delta_t}_{record.hash}_{record.groundtruth}_{idx}.npy'), ecg_segment.astype(np.float32))
+
+    fig, ax = plt.subplots(1, 1, figsize=(len(norm_ecg)/(record.fs), 4), dpi=200)
+    ax.plot(np.arange(0, len(norm_ecg)), norm_ecg, color='black', label="ECG Signal")
+
+    ymax = np.max(norm_ecg)
+    ymin = np.min(norm_ecg)
+
+    for (onset, offset) in segments:
+        qrscolor = '#f1c40f' 
+        ax.axvspan(onset, offset, ymax = ymax, ymin = ymin, color=qrscolor, alpha=0.5)
 
     ax.set_ylabel("Amplitude (mV)")
     ax.set_xlabel("Time (s)")
@@ -78,9 +97,17 @@ if __name__ == "__main__":
     argparser = argparse.ArgumentParser(description="Load and analyze ECG cases")
     argparser.add_argument("--input_path", type=str, help="Path to the input directory", required=True)
     argparser.add_argument("--batch_size", type=int, help="Number of cases to process in each batch", default=5)
+    argparser.add_argument("--out_dir", type=str, help="Path to the output directory", required=True)
     args = argparser.parse_args()
 
     batch_size = args.batch_size
+    out_dir = args.out_dir
+    segment_type = os.path.splitext(args.input_path)[0].split('_')[-1]
+    split = os.path.splitext(args.input_path)[0].split('_')[-2]
+    print(f'Processing {split} split for {segment_type} segments')
+    out_dir = os.path.join(out_dir, split, segment_type)
+    os.makedirs(out_dir, exist_ok=True)
+
     df = pd.read_csv(args.input_path, nrows=5)
 
     cur_batch = []
@@ -113,6 +140,6 @@ if __name__ == "__main__":
             records = [_rec for _rec, _, _ in cur_batch]
             analyse_single_case(records)
             for idx, _rec in enumerate(records):
-                plot_segment(_rec, cur_batch[idx][1], cur_batch[idx][2])
+                segment(_rec, cur_batch[idx][1], cur_batch[idx][2], segment_type, out_dir)
 
             cur_batch = []
