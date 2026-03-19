c366bdcc0c368b95364c14ecc073771ca762bd72
Tyme Chatupanyachotikul <122781615+tymechatupanyachotikul@users.noreply.github.com>
2026-03-19 10:11:25 +0100

    update aladin preprocess to include ukbb dataset

--- MoNODE/data/aladin_preprocess.py
+++ MoNODE/data/aladin_preprocess.py
@@ -18,9 +18,16 @@ from aladin.core import Record
 import matplotlib.pyplot as plt
 from pprint import pprint 
 
-MEDALCARE_XL_LEADS = ["I", "II", "III", "aVR", "aVL", "aVF", "V1", "V2", "V3", "V4", "V5", "V6"]
 
-def load_and_convert_case(row):
+
+MEDALCARE_XL_LEADS = ["I", "II", "III", "aVR", "aVL", "aVF", "V1", "V2", "V3", "V4", "V5", "V6"]
+UK_BB_LEADS    = ['I', 'II', 'III', 'aVR', 'aVL', 'aVF', 'V1', 'V2', 'V3', 'V4', 'V5', 'V6']
+LEADS_DICT = {
+    'MedalCare-XL': MEDALCARE_XL_LEADS,
+    'ukbb': UK_BB_LEADS
+}
+
+def load_and_convert_case(row, dataset):
     """Worker function to handle file checking, conversion, and loading."""
     ecg_path = str(row.data_path)
     directory_path, filename = os.path.split(ecg_path)
@@ -28,11 +35,12 @@ def load_and_convert_case(row):
     
     filepath = os.path.join(directory_path, case)
 
-    # Convert if missing
     if not os.path.exists(filepath + '.dat') or not os.path.exists(filepath + '.hea'):
-        ecg = pd.read_csv(ecg_path, header=None, dtype=np.float32).to_numpy()
-        
-        # Ensure shape is (samples, leads)
+        if filename.endswith('.csv'):
+            ecg = pd.read_csv(ecg_path, header=None, dtype=np.float32).to_numpy()
+        elif filename.endswith('.npy'):
+            ecg = np.load(ecg_path)
+
         if ecg.shape[0] < ecg.shape[1]:
             ecg = ecg.T
             
@@ -41,23 +49,25 @@ def load_and_convert_case(row):
             write_dir=directory_path,
             fs=500, 
             units=['mV'] * ecg.shape[1], 
-            sig_name=MEDALCARE_XL_LEADS[:ecg.shape[1]], 
+            sig_name=LEADS_DICT[dataset][:ecg.shape[1]], 
             p_signal=ecg, 
             fmt=['32'] * ecg.shape[1]
         )    
 
-    # Load record
     rec = wfdb.rdrecord(filepath)
     ecg_dict = {name: rec.p_signal[:, i] for i, name in enumerate(rec.sig_name)}
 
     record = Record(ecg_dict, rec.fs, "DEMO", case)
-    record.groundtruth = row.label 
-    record.hash = row.hash        
+    if hasattr(row, 'label'):
+        record.groundtruth = row.label 
+    if hasattr(row, 'hash'):
+        record.hash = row.hash        
     record.original_file_path = row.data_path
+
     return record, rec
 
-def process_and_save_segments(record, original_record, segment_type, out_dir, beat_type, plot=False):
-    """Worker function to handle normalization and saving to disk."""
+def get_ecg_segments_idx(record, segment_type, beat_type):
+
     segments = []
     
     if segment_type == 'ventricular':
@@ -71,16 +81,13 @@ def process_and_save_segments(record, original_record, segment_type, out_dir, be
                     continue
                 qrst_idx.append((start, end))
                 
-            # Safe sample in case there are fewer than 2 segments
             k = min(2, len(qrst_idx))
             segments = random.sample(qrst_idx, k) if k > 0 else []
         elif beat_type == 'median':
-            start = int(record.median_beat.delineations.qrs.onset)
-            end = int(record.median_beat.delineations.t.offset)
+            onset = record.median_beat.delineations.qrs.onset
+            offset = record.median_beat.delineations.t.offset
 
-            segments = [(start, end)]
-            if start is None or end is None:
-                return # Skip if median beat is not properly delineated
+            segments = [(int(onset), int(offset))] if onset is not None and offset is not None else []
         
     elif segment_type == 'atrial':
         if beat_type == 'sampled':
@@ -88,68 +95,93 @@ def process_and_save_segments(record, original_record, segment_type, out_dir, be
             sampled_p = random.sample(record.p, k) if k > 0 else []
             segments = [(int(seg.onset), int(seg.offset)) for seg in sampled_p]
         elif beat_type == 'median':
-            start = int(record.median_beat.delineations.p.onset)
-            end = int(record.median_beat.delineations.p.offset)
+            onset = record.median_beat.delineations.p.onset
+            offset = record.median_beat.delineations.p.offset
 
-            segments = [(start, end)]
-            if start is None or end is None:
-                return # Skip if median beat is not properly delineated
+            segments = [(int(onset), int(offset))] if onset is not None and offset is not None else []
 
-    if not segments:
-        return # Skip if no valid segments found
+    return segments 
 
-    original_ecg = original_record.p_signal if beat_type == 'sampled' else record.median_beat.ecg.T
-    print(f'ECG for {beat_type} {segment_type} has shape {original_ecg.shape}')
+def save_ecg_segment(segments, norm_ecg, original_record, record, segment_type, out_dir, beat_type, dataset, plot=False):
 
-    mu = np.mean(original_ecg, axis=0, keepdims=True)
-    sigma = np.std(original_ecg, axis=0, keepdims=True)
-    norm_ecg = (original_ecg - mu) / (sigma + 1e-8)
-
-    save_dir = os.path.join(out_dir, beat_type)
+    save_dir = os.path.join(out_dir, segment_type, beat_type)
     os.makedirs(save_dir, exist_ok=True)
-    
+
     for idx, (start, end) in enumerate(segments):
         ecg_segment = norm_ecg[start:end, :]
         delta_t = end - start
         
-        run_id = record.original_file_path.split('/')[-2].split('_')[1]
-        session_id = record.original_file_path.split('/')[-1].split('_')[0]
-        label = record.groundtruth.replace('.', '')
+        if dataset == 'MedalCare-XL':
+            run_id = record.original_file_path.split('/')[-2].split('_')[1]
+            session_id = record.original_file_path.split('/')[-1].split('_')[0]
+            label = record.groundtruth.replace('.', '')
 
-        if beat_type == 'sampled':
-            base_name = f'T{delta_t}_{run_id}_{session_id}_{label}_{idx}'
-        else:
-            base_name = f'T{delta_t}_{run_id}_{session_id}_{label}'
+            if beat_type == 'sampled':
+                base_name = f'T{delta_t}_{run_id}_{session_id}_{label}_{idx}'
+            else:
+                base_name = f'T{delta_t}_{run_id}_{session_id}_{label}'
+        elif dataset == 'ukbb':
+            run_id = os.path.splitext(record.original_file_path)[0]
+            if beat_type == 'sampled':
+                base_name = f'T{delta_t}_{run_id}_{idx}'
+            else:
+                base_name = f'T{delta_t}_{run_id}'
 
         np.save(os.path.join(save_dir, f'{base_name}.npy'), ecg_segment.astype(np.float32))
 
         if plot:
             if beat_type == 'median':
-                plt.figure(figsize=(10, 4))
-                plt.plot(original_record.p_signal[:, 0], label='Original ECG')
-                plt.xlabel('Time')
-                plt.ylabel('Amplitude (mV)')
-                plt.legend()
-                plt.savefig(f'{base_name}_full_ecg_{beat_type}.png')
-                plt.close() 
+                fig, ax = plt.subplots(figsize=(10, 4))
+                ax.plot(original_record.p_signal[:, 0], label='Original ECG')
+                ax.set_xlabel('Time')
+                ax.set_ylabel('Amplitude (mV)')
+                ax.legend()
+                fig.savefig(f'{base_name}_full_ecg_{beat_type}.png')
+                plt.close(fig) 
 
-                plt.figure(figsize=(10, 4))
-                plt.plot(norm_ecg[:, 0], label='Normalised median beat')
-                plt.axvspan(start, end, color='#e74c3c', alpha=0.5, label=f"Segmented for {segment_type}")
-                plt.xlabel('Time')
-                plt.ylabel('Amplitude')
-                plt.legend()
-                plt.savefig(f'{base_name}_norm_segment_{beat_type}.png')
-                plt.close() 
+                fig, ax = plt.subplots(figsize=(10, 4))
+                ax.plot(norm_ecg[:, 0], label='Normalised median beat')
+                ax.axvspan(start, end, color='#e74c3c', alpha=0.5, label=f"Segmented for {segment_type}")
+                ax.set_xlabel('Time')
+                ax.set_ylabel('Amplitude')
+                ax.legend()
+                fig.savefig(f'{base_name}_norm_segment_{beat_type}.png')
+                plt.close(fig) 
 
-                plt.figure(figsize=(10, 4))
-                plt.plot(record.median_beat.ecg[0, :], label='Median beat')
-                plt.axvspan(start, end, color='#e74c3c', alpha=0.5, label=f"Segmented for {segment_type}")
-                plt.xlabel('Time')
-                plt.ylabel('Amplitude (mV)')
-                plt.legend()
-                plt.savefig(f'{base_name}_segment_{beat_type}.png')
-                plt.close()
+                fig, ax = plt.subplots(figsize=(10, 4))
+                ax.plot(record.median_beat.ecg[0, :], label='Median beat')
+                ax.axvspan(start, end, color='#e74c3c', alpha=0.5, label=f"Segmented for {segment_type}")
+                ax.set_xlabel('Time')
+                ax.set_ylabel('Amplitude (mV)')
+                ax.legend()
+                fig.savefig(f'{base_name}_segment_{beat_type}.png')
+                plt.close(fig)
+
+def process_and_save_segments(record, original_record, segment_type, out_dir, beat_type, dataset, normalise=True, plot=False):
+    """Worker function to handle normalization and saving to disk."""
+    segments_dict = {}
+    segment_type = ['atrial', 'ventricular'] if segment_type == 'both' else [segment_type]
+
+    for seg_type in segment_type:
+        segment_idx = get_ecg_segments_idx(record, seg_type, beat_type)
+        if segment_idx:
+            segments_dict[seg_type] = segment_idx
+
+    if len(segments_dict.keys()) == 0:
+        return
+
+    original_ecg = original_record.p_signal if beat_type == 'sampled' else record.median_beat.ecg.T
+    print(f'ECG for {beat_type} {segment_type} has shape {original_ecg.shape}')
+
+    if normalise:
+        mu = np.mean(original_ecg, axis=0, keepdims=True)
+        sigma = np.std(original_ecg, axis=0, keepdims=True)
+        norm_ecg = (original_ecg - mu) / (sigma + 1e-8)
+    else:
+        norm_ecg = original_ecg
+
+    for seg_type, seg_idx in segments_dict.items():
+        save_ecg_segment(seg_idx, norm_ecg, original_record, record, seg_type, out_dir, beat_type, dataset, plot=plot)
     
 
 if __name__ == "__main__":
@@ -163,17 +195,28 @@ if __name__ == "__main__":
     argparser.add_argument("--plot_only", action='store_true', help="Only generate plots without saving segments")
     args = argparser.parse_args()
 
-    segment_type = os.path.splitext(args.input_path)[0].split('_')[-1]
+    dataset = os.path.basename(args.input_path).split('_')[0]
+
+    if dataset == 'MedalCare-XL':
+        segment_type = os.path.splitext(args.input_path)[0].split('_')[-1]
+        normalise = True
+    elif dataset == 'ukbb':
+        segment_type = 'both'
+        normalise = False
+
     beat_type = args.beat_type
-    split = os.path.splitext(args.input_path)[0].split('_')[-2]
+    if dataset == 'MedalCare-XL':
+        split = os.path.splitext(args.input_path)[0].split('_')[-2]
+    elif dataset == 'ukbb':
+        split = os.path.splitext(args.input_path)[0].split('_')[-1]
+
     demo = args.demo
     plot_only = args.plot_only
     print(f'Processing {split} split for {segment_type} segments')
     
-    out_dir = os.path.join(args.out_dir, split, segment_type)
+    out_dir = os.path.join(args.out_dir, split)
     os.makedirs(out_dir, exist_ok=True)
 
-    
     print("Loading ALADIN model into memory...")
     aladin = ALADIN(
         modelpaths=["ClassificationTrainer__nnUNetWithClassificationPlans__1d_decoding"],
@@ -190,7 +233,7 @@ if __name__ == "__main__":
         
         loaded_data = []
         with ThreadPoolExecutor(max_workers=args.workers) as executor:
-            future_to_row = {executor.submit(load_and_convert_case, row): row for row in chunk.itertuples(index=False)}
+            future_to_row = {executor.submit(load_and_convert_case, row, dataset): row for row in chunk.itertuples(index=False)}
             for future in as_completed(future_to_row):
                 try:
                     loaded_data.append(future.result())
@@ -211,20 +254,27 @@ if __name__ == "__main__":
             aladin.extract_median_beat_batch(records)
         
         if plot_only:
+            plot_dir = os.path.join(out_dir, segment_type)
+            os.makedirs(plot_dir, exist_ok=True)
+
             for record in records:
-                run_id = record.original_file_path.split('/')[-2].split('_')[1]
-                session_id = record.original_file_path.split('/')[-1].split('_')[0]
-                label = record.groundtruth.replace('.', '')
-                aladin.plot(record, name=os.path.join(out_dir, f'{run_id}_{session_id}_{label}_ecg.png'))
+                if dataset == 'MedalCare-XL':
+                    run_id = record.original_file_path.split('/')[-2].split('_')[1]
+                    session_id = record.original_file_path.split('/')[-1].split('_')[0]
+                    label = record.groundtruth.replace('.', '')
+                    aladin.plot(record, name=os.path.join(plot_dir, f'{run_id}_{session_id}_{label}_ecg.png'))
+                elif dataset == 'ukbb':
+                    run_id = os.path.splitext(record.original_file_path)[0]
+                    aladin.plot(record, name=os.path.join(plot_dir, f'{run_id}_ecg.png'))
         else:
             with ThreadPoolExecutor(max_workers=args.workers) as executor:
                 futures = []
                 for rec, orig in zip(records, original_records):
-                    futures.append(executor.submit(process_and_save_segments, rec, orig, segment_type, out_dir, beat_type, plot=demo))
+                    futures.append(executor.submit(process_and_save_segments, rec, orig, segment_type, out_dir, beat_type, dataset, normalise=normalise, plot=demo))
                 
                 for future in as_completed(futures):
                     try:
-                        future.result() # Catch any save errors
+                        future.result()
                     except Exception as e:
                         print(f"Error saving segment: {e}")
         
