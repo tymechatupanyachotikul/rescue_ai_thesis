bad19baab1b13aa3ae886cbcf777130d7949d871
Tyme Chatupanyachotikul <122781615+tymechatupanyachotikul@users.noreply.github.com>
2026-04-11 16:18:00 +0200

    .

--- MoNODE/data/check_dataset.py
+++ MoNODE/data/check_dataset.py
@@ -877,8 +877,23 @@ def refine_metadata_labels(metadata_path, split):
 
     print(f"refine_metadata_labels complete: {len(metadata)} UIDs processed → {metadata_path}")
 
-root_dir = '/projects/prjs1890/MedalCare-XL/segments'
-for split in ['train', 'valid', 'test']:
-    metadata_path = os.path.join(root_dir, f'{split}_metadata.json')
-    refine_metadata_labels(metadata_path, split)
+def fix_age(root_dir):
 
+    for split in ['train', 'valid', 'text']:
+        data_path = os.path.join(root_dir, f'{split}_metadata')
+        with open(data_path, 'r') as f:
+            data = json.load(f)
+
+        for uid, info in data.items():
+            age = info['labels'].get('age')
+
+            if isinstance(age, (int, float)):
+                age = 2026 - age 
+
+                data[uid]['labels']['age'] = age
+    
+        with open(data_path, 'w') as f:
+            json.dump(data, f, indent=4)
+
+
+fix_age('/home/tchatupanyacho/project/uk_biobank/segments')
