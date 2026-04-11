78012c21a3beb01c0ca993128ef8afc45d03464c
Tyme Chatupanyachotikul <122781615+tymechatupanyachotikul@users.noreply.github.com>
2026-04-11 09:33:26 +0200

    .

--- MoNODE/inference_analysis.py
+++ MoNODE/inference_analysis.py
@@ -342,7 +342,8 @@ def _collect_sample_latents(dataloader, model, split, args):
                                     for j, col in enumerate(columns)}
                             
                         latent_tensors['z0'].append(z0[i].detach().cpu().numpy())
-                        latent_tensors['m'].append(m[i].detach().cpu().numpy())
+                        if m is not None:
+                            latent_tensors['m'].append(m[i].detach().cpu().numpy())
                         metadata_dict.append({
                             'filename':   filenames[i],
                             'uid': patient_ids[i],
