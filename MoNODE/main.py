5456ffab02c21f491f620b3b21e1838e573d9625
Tyme Chatupanyachotikul <122781615+tymechatupanyachotikul@users.noreply.github.com>
2026-04-15 14:24:13 +0200

    .

--- MoNODE/main.py
+++ MoNODE/main.py
@@ -33,7 +33,7 @@ parser.add_argument('--shuffle', type=eval, default=True,
                help='For Moving MNIST whetehr to shuffle the data')
 parser.add_argument('--dataset_root', type=str, default='/projects/prjs1890/',
                     help="dataset location for ecg")
-parser.add_argument('--segment_type', choices=['atrial', 'ventricular'],
+parser.add_argument('--segment_type', choices=['atrial', 'ventricular', 'whole'],
                     help="Segment type of heart beat", type=str)
 parser.add_argument('--dataset', type=str, default='MedalCare-XL', 
                     help='Dataset to use')
